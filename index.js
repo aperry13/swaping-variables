851635b64674d52868893b2c7fa8bf7d8b599ae1
aperry13 <37085856+aperry13@users.noreply.github.com>
2019-05-23 14:42:03 -0600

    update

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ b = c;
 
 console.log("This is the a variable (red):", a);
 console.log("This is the b variable (blue):", b);
+
+//for -in
+const person ={
+ name: "allen",
+ age: 18
+};
+
+for  (let key in person)
+console.log (key,  person[key]);
+
+const colors = ['red', 'green', 'blue'];
+
+for (let index in colors)
+console.log(index, colors[index]);
+
+// for-of
+for (let color of colors)
+console.log(color);
